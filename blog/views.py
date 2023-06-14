de56d51a1e95a306eb89994ed42c9c587af81562
Luisa Santos <luisavtrsantos@gmail.com>
2023-06-14 11:28:58 -0300

    ola django

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+#inclui a classe httpresponde.
+from django.http import HttpResponse
+
+#define uma function view chamada index.
+def index(request):
+    return HttResponse('Olá Django - index')
+
+#define uma function view chamada ola
+def ola(request):
+    return HttpResponse('Olá Django')
+
+
