8c204a7e7a7ec1bebef5877ed653b179081e475b
Lu <luisavtrsantos@gmail.com>
2023-06-28 11:20:37 -0300

    atualizaas

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,12 @@ from django.http import HttpResponse
 
 #define uma function view chamada index.
 def index(request):
-    return HttpResponse('Olá Django - index')
+    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 
 #define uma function view chamada ola
 def ola(request):
     return HttpResponse('Olá Django')
 
-def index(request):
-    return render(request, 'index.html')
+# def index(request):
+#     return render(request, 'index.html')
 
