34cd38294e06f3af33802deafa20ab0bea3a2def
Maluh <luisavtrsantos@gmail.com>
2023-08-09 10:59:29 -0300

    atualizado09/08

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 #inclui a classe httpresponde.
 from django.http import HttpResponse
 
+from blog.models import Post
+
 #define uma function view chamada index.
 def index(request):
     return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
@@ -10,7 +12,10 @@ def index(request):
 #define uma function view chamada ola
 def ola(request):
     #return HttpResponse('Olá Django')#
-    return render(request, 'home.html')
+    #return render(request, 'home.html')#
+    posts = Post.objects.all() # recupera todos os posts do banco de dados
+    context = {'posts_list': posts } # cria um dicionário com os dado
+    return render(request, 'posts.html', context) # renderiza o template e passa o contexto
 
 # def index(request):
 #     return render(request, 'index.html')
