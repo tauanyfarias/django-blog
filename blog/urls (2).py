f60047809913ff65e96406c224d38c3dcfa554ab
tauanyfarias <cg3016161@LAB204M31.IFSP>
2023-09-20 11:05:07 -0300

    mensagem

---
+++ blog/urls (2).py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from blog.views import (index, ola ,post_show, PostDetailView, get_all_posts ,get_post, PostCreateView, create_post)
+
+
+urlpatterns = [
+    path('index/', index, name= "index"), # Definir a rota /index
+    path('ola/', ola, name="ola"), # Definir a rota /ola
+    path('posts/all', ola, name="posts_list"),
+    path('post/<int:post_id>', post_show, name="exibe_post"),
+    path('post/<int:pk>/show', PostDetailView.as_view(), name="post_detail"),
+    path('api/posts', get_all_posts, name="posts_data"),
+    path('api/posts/<int:post_id>', get_post, name="post_data"),path('post/add', PostCreateView.as_view(), name="post_add"),
+    path('api/post/add', create_post, name="create_post_data"),
+
+
+]
